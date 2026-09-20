ad51a5168c342c96219b2114b1f65e7c4ecd4b16
David Rodriguez <dr507498@gmail.com>
2026-09-20 18:04:30 -0400

    SODA utility works

--- notebooks/utils/soda3_client.py
+++ notebooks/utils/soda3_client.py
@@ -5,9 +5,9 @@ import requests
 
 
 class SOQL_Querying:
-    def __init__(self, dataset_url: str, app_token: str = None):
-        domain = re.search(r"https?://([^/]+)", dataset_url).group(1)
-        view_id = re.search(r"([a-z0-9]{4}-[a-z0-9]{4})", dataset_url, re.I).group(1)
+    def __init__(self, dataset_url: str, app_token: str):
+        domain = re.search(r"https?://([^/]+)", dataset_url).group(1)  # type: ignore
+        view_id = re.search(r"([a-z0-9]{4}-[a-z0-9]{4})", dataset_url, re.I).group(1)  # type: ignore
 
         self.endpoint = f"https://{domain}/api/v3/views/{view_id}/query.json"
         self.headers = {"X-App-Token": app_token} if app_token else {}
